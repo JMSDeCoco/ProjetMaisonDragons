37206dfa671aee1e1e80c4a1e3b85b687fc10e3a
NicolasDENISIPSSI <n.denis@ecole-ipssi.net>
2021-02-11 18:00:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-koagh1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-koagh1)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div className="APP">
+      <h1>Bienvenue dans maison&dragons</h1>
+      <h2>accueil</h2>
+      <h1>Test Denis</h1>
+    </div>
+  );
+}
